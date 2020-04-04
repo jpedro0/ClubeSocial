406948c5d7334f5e1ba2381770a1f5078faa0c34
JPedro Brito <jpedro0brito@gmail.com>
2020-04-04 12:47:12 -0400

    [Tarefa] Adicionando o projeto MVC [NEW] Add projeto [NEW] classe Socio [NEW] Enum Situacao

---
+++ ClubeSocial/Models/Socio.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClubeSocial.Models
+{
+    public class Socio
+    {
+        public Situacao Situacao { get; set; }
+    }
+}
